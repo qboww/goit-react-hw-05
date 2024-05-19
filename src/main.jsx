dbd14fc5a2c2fc3001d22d7d6261d020bac2b469
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-19 22:27:28 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ReactModal from "react-modal";
 
 import "modern-normalize";
 import "./styles/index.css";
 
-import App from "./components/App/App";
-
-// Set the root app element for accessibility
-ReactModal.setAppElement("#root");
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
