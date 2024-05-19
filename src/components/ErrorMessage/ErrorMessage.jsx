ae24f70e493d7a8d04fbe2985bba946ede097589
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-19 22:17:23 +0300

    initial commit

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import css from "./ErrorMessage.module.css";
-
-const ErrorMessage = ({ message }) => {
-  return <p className={css.errorMessage}>{message}</p>;
-};
-
-export default ErrorMessage;
